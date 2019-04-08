70ff3eb461979dd4574199107a3dee959e3aa9b5
Javier <jmartin@grupoloyal.com>
2019-04-08 18:21:17 +0200

    Demo1

---
+++ scripts.sql
@@ -0,0 +1,20 @@
+USE [CursoSSIS]
+GO
+
+/****** Object:  Table [Pelis].[Clientes]    Script Date: 08/04/2019 18:20:27 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [Pelis].[Clientes](
+	[idCliente] [int] IDENTITY(1,1) NOT NULL,
+	[Nombre] [nvarchar](255) NOT NULL,
+ CONSTRAINT [PK_Clientes] PRIMARY KEY CLUSTERED 
+(
+	[idCliente] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
