8a1e96f90429465d6dafd6144ab8aeb5f23280fd
Javier <jmartin@grupoloyal.com>
2019-04-09 09:44:44 +0200

    Alquileres

--- scripts.sql
+++ scripts.sql
@@ -18,3 +18,37 @@ CREATE TABLE [Pelis].[Clientes](
 ) ON [PRIMARY]
 GO
 
+/****** Object:  Table [Pelis].[Alquileres]    Script Date: 09/04/2019 9:43:41 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [Pelis].[Alquileres](
+	[IdAlquiler] [int] IDENTITY(1,1) NOT NULL,
+	[IdPelicula] [int] NOT NULL,
+	[IdCliente] [int] NOT NULL,
+	[FAlquiler] [datetime] NOT NULL,
+	[FDevolucion] [datetime] NULL,
+ CONSTRAINT [PK_Alquileres] PRIMARY KEY CLUSTERED 
+(
+	[IdAlquiler] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [Pelis].[Alquileres]  WITH CHECK ADD  CONSTRAINT [FK_Alquileres_Clientes] FOREIGN KEY([IdCliente])
+REFERENCES [Pelis].[Clientes] ([idCliente])
+GO
+
+ALTER TABLE [Pelis].[Alquileres] CHECK CONSTRAINT [FK_Alquileres_Clientes]
+GO
+
+ALTER TABLE [Pelis].[Alquileres]  WITH CHECK ADD  CONSTRAINT [FK_Alquileres_Peliculas] FOREIGN KEY([IdPelicula])
+REFERENCES [Pelis].[Peliculas] ([idPelicula])
+GO
+
+ALTER TABLE [Pelis].[Alquileres] CHECK CONSTRAINT [FK_Alquileres_Peliculas]
+GO
+
